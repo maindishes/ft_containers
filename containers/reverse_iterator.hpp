0e304819482c93320ece6dad0f61c7deef96d365
maindishes <manynolan1@gmail.com>
2022-08-31 13:50:49 +0900

    start : iterator

---
+++ containers/reverse_iterator.hpp
@@ -0,0 +1,16 @@
+namespace ft
+{
+template <class Iterator> 
+class reverse_iterator 
+{
+    public:
+        //Member_types
+        typedef Iterator iterator_type;
+           
+        typedef typename iterator_traits<Iterator>::iterator_category   iterator_category;
+        typedef typename iterator_traits<Iterator>::value_type          value_type;
+        typedef typename iterator_traits<Iterator>::difference_type     difference_type;
+        typedef typename iterator_traits<Iterator>::pointer             pointer;
+        typedef typename iterator_traits<Iterator>::reference           reference;
+};
+}
