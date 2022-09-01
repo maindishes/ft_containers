5f2e8e74684b4ef1cb536046e00ef4858d833771
maindishes <manynolan1@gmail.com>
2022-09-01 15:39:26 +0900

    Add: reverse_iterator member function overloads

--- containers/reverse_iterator.hpp
+++ containers/reverse_iterator.hpp
@@ -22,9 +22,98 @@ class reverse_iterator: public ft::iterator<typename ft::iterator_traits<Iterato
         typedef typename iterator_traits<Iterator>::reference           reference;
     
     protected:
-        iterator_type current_state;
+        iterator_type current_state; // A copy of the original iterator
     
     public:
+    // 생성자
+        reverse_iterator() {} // default (1)
+        
+        explicit reverse_iterator(iterator_type it); 
+        : current_state(it) {} // initializtion (2)
+        
+        // reverse_iterator(const reverse_iterator& __x)
+        // : _M_current(__x._M_current) {}
+
+        template<class Iter>       // copy (3)
+        reverse_iterator(const reverse_iterator<Iter>& rev_it)
+        : current_state(rev_it.base()) {}
+    // 소멸자
+        virtual ~reverse_iterator() {}
+        
+        // return base iterator
+        iterator_type base() const
+        {
+            return current_state;
+        }
+
+        //operators
+        reference operator*() const
+        {
+            iterator_type tmp = current_state;
+            return *(--tmp);
+        } 
+
+        //pointer
+        pointer operator->() const
+        {
+            return &(operator*());
+        }
+
+        reverse_iterator& operator++()
+        {
+            --current_state;
+            return *this;
+        }
+
+        reverse_iterator operator++(int)
+        {
+            reverse_iterator __tmp = *this;
+            --current_state;
+            return __tmp;
+        }
+
+        reverse_iterator& operator--()
+        {
+            ++current_state;
+            return *this;
+        }
+
+        reverse_iterator operator--(int)
+        {
+            reverse_iterator __tmp = *this;
+            ++current_state;
+            return __tmp;
+        }
+
+        reverse_iterator operator+(difference_type __n) const
+        {
+            return reverse_iterator(current_state - __n);
+        }
+
+        reverse_iterator& operator+=(difference_type __n)
+        {
+            current_state -= __n;
+            return *this;
+        }
+
+        reverse_iterator operator-(difference_type __n) const
+        {
+            return reverse_iterator(current_state + __n);
+        }
+
+        reverse_iterator& operator-=(difference_type __n)
+        {
+            current_state += __n;
+            return *this;
+        }
+
+        reference operator[](difference_type __n) const
+        {
+            return *(*this + __n);
+        }
 };
+
+// Non-member function overloads
+// relational operators
 }
 #endif
