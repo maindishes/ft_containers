fc8157b975f27e88a8834eee3d4dcbc1852cd3b2
maindishes <manynolan1@gmail.com>
2022-09-03 20:36:42 +0900

    template study

--- containers/test.cpp
+++ containers/test.cpp
@@ -2,17 +2,114 @@
 #include <string>
 
 template <typename T>
-T max(T& a, T& b)
+class Vector
 {
-    return a>b ? a : b;
+    T* data;
+    int capacity;
+    int length;
+
+    public:
+        // 어떤 타입을 보관 하는지
+        typedef T value_type;
+        
+        // 생성자
+        Vector(int n =1): data(new T[n]), capacity(n), length(0)
+        {
+        }
+
+        void push_back(T  s)
+        {
+            if (capacity <= length)
+            {
+                T * temp = new T [capacity*2];
+                for (int i =0; i < length; i++)
+                {
+                    temp[i] = data[i];
+                }
+                delete[] data;
+                data = temp;
+                capacity *= 2;
+            }
+            data[length] = s;
+            length++;
+        }
+        // 임의의 위치 원소에 접근
+        T operator[](int i)
+        {
+            return data[i];
+        }
+
+        void remove(int x)
+        {
+            
+            for (int i = x+1; i < length; i++)
+            {
+                data[i-1] = data[i];
+            }
+            length--;
+        }
+        // swap
+        void swap(int i, int j)
+        {
+            T temp = data[i];
+            data[i] = data[j];
+            data[j] = temp;
+        }
+
+        void print()
+        {
+            for (int i = 0; i < length; i++)
+            {
+                std::cout << "[ " <<data[i] << " ]" ;
+            }
+            std::cout << std::endl;
+        }
+        int size()
+        {
+            return length;
+        }
+
+        virtual ~Vector()
+        {
+            if (data)
+            {
+                delete[] data;
+            }
+        }
+
+};
+
+template <typename Cont>
+void bubble_sort(Cont& cont)
+{
+    for (int i =0; i < cont.size(); i++)
+    {
+        for (int j= i+1; j<cont.size(); j++)
+        {
+            if (cont[i] > cont[j])
+            {
+                cont.swap(i,j);
+            }
+        }
+    }
 }
+
 
 int main()
 {
-    int a = 1, b = 2;
-    std::cout << "Max (" << a << "," << b << ") ? : " << max(a,b) << std::endl;
+    Vector<int> int_vec;
+    int_vec.push_back(3);
+    int_vec.push_back(333);
+    int_vec.push_back(33);
+    int_vec.push_back(3333);
+    int_vec.push_back(33333);
+    int_vec.push_back(311);
 
-    std::string s = "hello", t = "world";
-    std::cout << "Max (" << s << "," << t << ") ? : " << max(s,t) << std::endl;
+    std::cout << "정렬 이전 ---- " << std::endl;
+    int_vec.print();
+
+    std::cout << " 정렬 이후 ---- " << std::endl;
+    bubble_sort(int_vec);
+    int_vec.print();
+
 }
-
